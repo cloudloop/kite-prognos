965d02458af6a2b8e2a89aee945029701b0c7500
Cloudloop <axel.ingo@me.com>
2022-09-23 15:55:40 +0200

    "Basic API call to SMHI API for lat/lon position"

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from pip._vendor import requests
+import json
+#from requests import requests
 
+###
+#Link to documentation https://opendata.smhi.se/apidocs/metfcst/get-forecast.html
+
+
+#Point of interest: Enter location you want forcast for
+lon = 59
+lat = 18
+
+#Setting up API call function for SMHI forcast on location
+#See https://opendata.smhi.se/apidocs/metfcst/parameters.html
+smhi_url = "https://opendata-download-metfcst.smhi.se"
+Sthlm_Lat = "59.368718"
+Sthlm_Lon = "18.200225"
+full_url = smhi_url+f"/api/category/pmp3g/version/2/geotype/point/lon/{Sthlm_Lon}/lat/{Sthlm_Lat}/data.json"
+response = requests.get(full_url)
+response.raise_for_status()
+data = response.json()
+#Printing readable version of json data. 
+print(json.dumps(data, indent=4))
